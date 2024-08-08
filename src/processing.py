eb89f6777d234445540eb983e4d48277e300fa02
AMaldu <anamld62@gmail.com>
2024-08-08 15:50:59 +0200

    refatoring and added EDA notebooks

--- src/processing.py
+++
@@ -1,8 +0,0 @@
-
-
-
-
-df_imputed_outliers = data.copy()
-for column in columns_to_check:
-    median = data[column].median()
-    df_imputed_outliers.loc[outliers_indices[column], column] = median
