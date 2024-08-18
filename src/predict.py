d71390878fe5240c6e8b09634ee2ac6ffeb719a9
AMaldu <anamld62@gmail.com>
2024-08-18 18:32:15 +0200

    multivariate analysis modified

--- src/predict.py
+++ src/predict.py
@@ -1,31 +1,66 @@
-
+import os
 import pickle
 from typing import Any
 from flask import Flask, request, jsonify
-
-def load_and_predict_model(X_test: Any, filename='pipeline_model.pkl'):
-    with open(filename, 'rb') as file:
-        loaded_pipeline = pickle.load(file)
-    y_pred_loaded = loaded_pipeline.predict(X_test)    
-    return y_pred_loaded
+import pandas as pd
 
 
-app = Flask('fraud-prediction')
+# run gunicorn --bind=0.0.0.0:9696 src.predict:app
 
 
-@app.route('/predict', methods = ['POST'])
-def predict_endpoit():
+def load_and_predict_model():
+    base_path = os.path.dirname(__file__)  # Obtiene el directorio actual
+    model_path = os.path.join(base_path, '../orchestration/models/random_forest_classifier.b')
+    prep_path = os.path.join(base_path, '../orchestration/models/preprocessor_pipeline.b')
+    
+    # Carga el modelo
+    with open(model_path, "rb") as f_in:
+        model = pickle.load(f_in)
+
+    # Carga el preprocesador
+    with open(prep_path, "rb") as f_in:
+        preprocessor = pickle.load(f_in)
+
+    return preprocessor, model
+
+preprocessor, model = load_and_predict_model()
+
+def prepare_features(customer: dict) -> pd.DataFrame:
+    features = {
+        'step': customer['step'],
+        'type': customer['type'],
+        'amount': customer['amount'],
+        'nameOrig': customer['nameOrig'],
+        'oldbalanceOrig': customer['oldbalanceOrig'],
+        'newbalanceOrig': customer['newbalanceOrig'],
+        'nameDest': customer['nameDest'],
+        'oldbalanceDest': customer['oldbalanceDest'],
+        'newbalanceDest': customer['newbalanceDest'],
+        'diffbalanceOrig': customer['newbalanceOrig'] - customer['oldbalanceOrig'],
+        'diffbalanceDest': customer['newbalanceDest'] - customer['oldbalanceDest']
+    }
+    
+    return pd.DataFrame([features])
+
+def predict(features: pd.DataFrame) -> Any:
+    X = preprocessor.transform(features)
+    preds = model.predict(X)
+    return preds
+
+app = Flask(__name__)
+
+@app.route('/predict', methods=['POST'])
+def predict_endpoint():
     customer = request.get_json()
-    pred = load_and_predict_model(customer)
+    features_df = prepare_features(customer)
+    pred = predict(features_df)
     
     result = {
-        'isFraud':pred
+        'isFraud': int(pred[0])
     }
 
     return jsonify(result)
 
-
-
-
 if __name__ == "__main__":
-    app.run(debug = True, host = '0.0.0.0', port = 9696)
+    app.run(debug=True, host='0.0.0.0', port=9696)
+
--- src/test.py
+++ src/test.py
@@ -1,16 +1,26 @@
 import requests
+from predict import predict, prepare_features
+import pandas as pd
 
 customer = {
     'step': 5,
-    'type': CASH_OUT,
+    'type': 'CASH_OUT',
     'amount': 23453.6,
-	'oldbalanceOrig': 0.0,
-    'newbalanceOrig': 181.0,
-    'nameDest': 235.860018,
+    'nameOrig':'C1231006815',
+	'oldbalanceOrig': 181.0,
+    'newbalanceOrig': 160296.359375,
+    'nameDest': 'M1979787155',
     'oldbalanceDest': 21182.00000,
     'newbalanceDest': 214661.4375
     
 }
+customer_df = prepare_features(customer)
+
+
+pred = predict(customer_df)
+print(pred[0])
+
+
 
 url = 'http://localhost:9696/predict'
 
