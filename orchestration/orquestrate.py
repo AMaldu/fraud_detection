79c07b5aba9b4355c32b140d1c343e20ba31bb67
AMaldu <anamld62@gmail.com>
2024-08-18 18:32:43 +0200

    orchestration modified with SMOTE

--- orchestration/orquestrate.py
+++ orchestration/orquestrate.py
@@ -8,7 +8,7 @@ from typing import Tuple
 from scipy.sparse import csr_matrix
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.pipeline import Pipeline
+from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
 import pickle
 from sklearn.ensemble import RandomForestClassifier
@@ -19,7 +19,12 @@ def ingest_files() -> pd.DataFrame:
     """Read data into dataframe"""
     file_path = 'data/bronze/PS_20174392719_1491204439457_log.csv'
     
-    df = pd.read_csv(file_path, nrows = 1000)
+    df = pd.read_csv(file_path)
+    
+    step_counts = df['step'].value_counts()
+    valid_steps = step_counts[step_counts >= 2].index
+    filtered_df = df[df['step'].isin(valid_steps)]
+    df = filtered_df.groupby('step').sample(n=2, random_state=42)
     
     return df
 
@@ -49,40 +54,43 @@ def combine_features(df: DataFrame) -> DataFrame:
 
 @task
 def select_features(df: pd.DataFrame, target: str) -> pd.DataFrame:
-    # Identifica columnas numéricas y categóricas
-    numerical_cols = df.select_dtypes(include=['float32', 'int16', 'int8', 'int64', 'float64']).columns
-    categorical_cols = df.select_dtypes(include=['object', 'category']).columns
+    # Crear una copia del DataFrame para no modificar el original
+    df_copy = df.copy()
 
-    # Verifica si la columna target está en el DataFrame
-    if target not in df.columns:
+    # Identificar columnas numéricas y categóricas
+    numerical_cols = df_copy.select_dtypes(include=['float32', 'int16', 'int8', 'int64', 'float64']).columns
+    categorical_cols = df_copy.select_dtypes(include=['object', 'category']).columns
+
+    # Verificar si la columna target está en el DataFrame
+    if target not in df_copy.columns:
         raise ValueError(f"Target column '{target}' not found in DataFrame columns.")
     
-    # Si target está en columnas numéricas, lo excluye de la lista de columnas numéricas
+    # Excluir la columna target de las columnas numéricas
     if target in numerical_cols:
         numerical_cols = numerical_cols[numerical_cols != target]
-    
-    # Codifica variables categóricas usando TargetEncoder
-    encoder = ce.TargetEncoder(cols=categorical_cols)
-    df_encoded = encoder.fit_transform(df, df[target])
 
-    # Calcula la matriz de correlación
+    # Codificar variables categóricas usando TargetEncoder
+    encoder = ce.TargetEncoder(cols=categorical_cols)
+    df_encoded = encoder.fit_transform(df_copy, df_copy[target])
+
+    # Calcular la matriz de correlación en el DataFrame codificado
     correlation_matrix = df_encoded.corr()
 
-    # Identifica pares de columnas con alta correlación
+    # Identificar pares de columnas con alta correlación
     threshold = 0.9
     high_correlation_pairs = [(col1, col2) for col1 in correlation_matrix.columns for col2 in correlation_matrix.columns 
                               if col1 != col2 and abs(correlation_matrix.loc[col1, col2]) > threshold]
 
-    # Establece las variables a eliminar, pero siempre conserva la columna target
+    # Establecer las variables a eliminar, pero siempre conserva la columna target
     variables_to_remove = set()
     for col1, col2 in high_correlation_pairs:
         if col1 != target and col2 != target:
             if col1 not in variables_to_remove and col2 not in variables_to_remove:
-                variables_to_remove.add(col2)
+                variables_to_remove.add(col2)  # Elegir una de las dos columnas para eliminar
 
-    # Elimina columnas altamente correlacionadas, pero conserva la columna target
-    df = df_encoded.drop(columns=variables_to_remove, errors='ignore')
-    
+    # Eliminar las columnas altamente correlacionadas del DataFrame original
+    df = df_copy.drop(columns=variables_to_remove, errors='ignore')
+
     print("Columnas después de seleccionar características:", df.columns)
     
     return df
@@ -91,42 +99,49 @@ def select_features(df: pd.DataFrame, target: str) -> pd.DataFrame:
 def preprocessor(
     df: pd.DataFrame, target: str, test_size: float = 0.2, random_state: int = 42
 ) -> Tuple[csr_matrix, csr_matrix, pd.Series, pd.Series]:
-    """Preprocess data and split into train/test sets"""
-    categorical_features = ['type']
-    numeric_features = ['step', 'amount', 'oldbalanceOrig', 'oldbalanceDest', 'diffbalanceOrig', 'diffbalanceDest']
+
+    # Identificar características categóricas y numéricas
+    categorical_features = df.select_dtypes(include=['object', 'category']).columns.tolist()
+    numeric_features = df.select_dtypes(include=['int', 'float']).columns.tolist()
+    numeric_features.remove(target)  
+
+    print(f"Categorical features: {categorical_features}")
+    print(f"Numeric features: {numeric_features}")
     
     categorical_transformer = OneHotEncoder(handle_unknown='ignore', sparse_output=True)
     numeric_transformer = StandardScaler()
-    
+
+    # Preprocesador para características categóricas y numéricas
     preprocessor = ColumnTransformer(
         transformers=[
             ('num', numeric_transformer, numeric_features),
             ('cat', categorical_transformer, categorical_features)
         ]
     )
-    
-    pipeline = Pipeline(steps=[
-        ('preprocessor', preprocessor)
-    ])
-    
+
+    # Separar características y objetivo
     X = df.drop(columns=target)
     y = df[target]
-    
-    X_train, X_test, y_train, y_test = train_test_split(
-        X,
-        y,
-        test_size=test_size,
-        random_state=random_state
-    )
-    
-    X_train = pipeline.fit_transform(X_train)
-    X_test = pipeline.transform(X_test)
-    
-    pipeline_path = "orchestration/models/preprocessor_pipeline.b"
-    with open(pipeline_path, "wb") as f_out:
-        pickle.dump(pipeline, f_out)
-    
-    print(f"Pipeline guardado en '{pipeline_path}'.")
+
+    # Dividir los datos en conjuntos de entrenamiento y prueba
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+
+    # Ajustar el preprocesador en los datos de entrenamiento
+    X_train = preprocessor.fit_transform(X_train)
+
+    # Aplicar SMOTE para el re-muestreo de los datos (opcional)
+    smote = SMOTE(random_state=random_state)
+    X_train, y_train = smote.fit_resample(X_train, y_train)
+
+    # Transformar los datos de prueba
+    X_test = preprocessor.transform(X_test)
+
+    # Guardar el preprocesador
+    preprocessor_path = "orchestration/models/preprocessor_pipeline.b"
+    with open(preprocessor_path, "wb") as f_out:
+        pickle.dump(preprocessor, f_out)
+
+    print(f"Preprocesador guardado en '{preprocessor_path}'.")
     return X_train, X_test, y_train, y_test
 
 
